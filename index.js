44326932546346618921dd646eed4dbbd116f0f6
Will <willians.vargas@maximo.pe>
2021-07-10 22:52:27 +0200

    added cors

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
+var cors = require('cors')
 const app = express();
 const routes = require("./routes");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use(cors())
 
 app.use("/", routes);
 
